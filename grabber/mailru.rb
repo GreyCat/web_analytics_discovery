88c495b2f21bbb59656f33a1853053055b2237ea
Mikhail Yakshin <greycat@altlinux.org>
2013-07-21 02:15:00 +0400

    Added MailRu grabber

---
+++ grabber/mailru.rb
@@ -0,0 +1,53 @@
+require 'grabberutils'
+
+class MailRu
+	include GrabberUtils
+
+	def initialize(url)
+		@url = url
+	end
+
+	def run
+		@page = download(@url)
+		run_id(find_id)
+	end
+
+	def find_id
+		if @page =~ /<a href="http:\/\/top.mail.ru\/jump\?from=(\d+)"><img src="http:\/\/.*.top.mail.ru\/counter/
+			$1
+		else
+			nil
+		end
+	end
+
+	def run_id(id)
+		return nil unless id
+		r = {}
+
+		doc = download("http://top.mail.ru/visits.csv?id=#{id}&period=0&date=&back=15&").split(/\n/)[4..-1]
+		sum_v = 0
+		sum_pv = 0
+		doc.each { |l|
+			#"Дата";"Посетители";"Новые посетители";"Ядро";"Хосты";"Просмотры";"Глубина"
+			date, v, new_v, core_v, hosts, pv, depth = l.split(/;/)
+			sum_v += v.to_i
+			sum_pv += pv.to_i
+		}
+
+		#doc = download("http://top.mail.ru/visits?id=#{id}")
+
+		r[:visitors_day] = sum_v / doc.size
+		r[:pv_day] = sum_pv / doc.size
+		return r
+	end
+
+	def grab(doc, classname)
+		a = []
+		doc.gsub(/#{classname}">([^<]+)<\/td>/) {
+			r = $1
+			r.gsub!(/&nbsp;/, '')
+			a << r.to_i
+		}
+		return a
+	end
+end
