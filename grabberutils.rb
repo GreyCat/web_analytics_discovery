073ae0a4675d108dc0c18b69f9eac407dbe1f680
Mikhail Yakshin <greycat@altlinux.org>
2013-07-21 01:33:10 +0400

    Added workaround for invalid utf8 problem with Ruby 2

--- grabberutils.rb
+++ grabberutils.rb
@@ -8,7 +8,7 @@ module GrabberUtils
 			system("wget --user-agent='Mozilla/5.0 (Windows NT 6.1; rv:22.0) Gecko/20100101 Firefox/22.0' -O'#{fn}' '#{url}'")
 			raise 'Download error' if $?.exitstatus != 0
 		end
-		return File.read(fn)
+		return File.read(fn).encode!('UTF-8', 'UTF-8', :invalid => :replace)
 	end
 
 	def mangle_url(url)
