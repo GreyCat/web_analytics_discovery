482520f8c14052c1b0f24f6a45693654650b2e22
Mikhail Yakshin <greycat@altlinux.org>
2013-07-20 21:32:08 +0400

    Grab all 6 variables from Openstat rating

--- grabber/openstat.rb
+++ grabber/openstat.rb
@@ -8,18 +8,18 @@ class Openstat
 	def run_id(id)
 		r = {}
 		doc = download("http://rating.openstat.ru/site/#{id}")
-		r[:visitors_day] = grab(doc, 'osb-rating_site-e-table-col-m-day')
-		r[:visitors_mon] = grab(doc, 'osb-rating_site-e-table-col-m-month')
+		r[:visitors_day], r[:visits_day], r[:pv_day] = grab(doc, 'osb-rating_site-e-table-col-m-day')
+		r[:visitors_mon], r[:visits_mon], r[:pv_mon] = grab(doc, 'osb-rating_site-e-table-col-m-month')
 		return r
 	end
 
 	def grab(doc, classname)
-		if doc =~ /#{classname}">([^<]+)<\/td>/
+		a = []
+		doc.gsub(/#{classname}">([^<]+)<\/td>/) {
 			r = $1
 			r.gsub!(/&nbsp;/, '')
-			r.to_i
-		else
-			nil
-		end
+			a << r.to_i
+		}
+		return a
 	end
 end
