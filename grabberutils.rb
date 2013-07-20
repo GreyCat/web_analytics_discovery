db9b82cea02f64cf1ba885fc616eedb0f438f261
Mikhail Yakshin <greycat@altlinux.org>
2013-07-21 02:43:04 +0400

    Added possibility to read non-UTF-8 files

--- grabberutils.rb
+++ grabberutils.rb
@@ -1,14 +1,14 @@
 module GrabberUtils
 	CACHE_DIR = 'cache'
 
-	def download(url)
+	def download(url, encoding = 'UTF-8')
 		FileUtils.mkdir_p(CACHE_DIR)
 		fn = CACHE_DIR + '/' + mangle_url(url)
 		unless FileTest.exists?(fn)
 			system("wget --user-agent='Mozilla/5.0 (Windows NT 6.1; rv:22.0) Gecko/20100101 Firefox/22.0' -O'#{fn}' '#{url}'")
 			raise 'Download error' if $?.exitstatus != 0
 		end
-		return File.read(fn).encode!('UTF-8', 'UTF-8', :invalid => :replace)
+		return File.read(fn).encode!('UTF-8', encoding, :invalid => :replace)
 	end
 
 	def mangle_url(url)
