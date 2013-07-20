fd6d8654b5de24aa9b0a081348995efb139b801c
Mikhail Yakshin <greycat@altlinux.org>
2013-07-21 03:25:02 +0400

    More intricate mail.ru code detection

--- grabber/mailru.rb
+++ grabber/mailru.rb
@@ -9,7 +9,7 @@ class MailRu
 	end
 
 	def find_id
-		if @page =~ /<a href="http:\/\/top.mail.ru\/jump\?from=(\d+)"><img src="http:\/\/.*.top.mail.ru\/counter/
+		if @page =~ /<a href="http:\/\/top.mail.ru\/jump\?from=(\d+)".*><img src="http:\/\/.*.top.mail.ru\/counter/
 			$1
 		else
 			nil
