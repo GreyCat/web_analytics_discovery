d0b73e535c6148e5949fec0bbaa7340f420d4439
Mikhail Yakshin <greycat@altlinux.org>
2013-07-21 05:00:48 +0400

    Added ugly hack against too clever Ruby 1.9.3

--- grabberutils.rb
+++ grabberutils.rb
@@ -8,7 +8,11 @@ module GrabberUtils
 			system("wget --user-agent='Mozilla/5.0 (Windows NT 6.1; rv:22.0) Gecko/20100101 Firefox/22.0' -O'#{fn}' '#{url}'")
 			raise 'Download error' if $?.exitstatus != 0
 		end
-		return File.read(fn).encode!('UTF-8', encoding, :invalid => :replace)
+
+		# Truly horrible hack to work around Ruby 1.9.2+ strict handling of invalid UTF-8 characters
+		s = File.read(fn)
+		s.encode!('UTF-16', encoding, :invalid => :replace, :replace => '?')
+		s.encode!('UTF-8', 'UTF-16', :invalid => :replace, :replace => '?')
 	end
 
 	def mangle_url(url)
