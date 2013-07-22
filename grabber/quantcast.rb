a12edaf447560dadd4eb67c31e55f5056586a9c5
Mikhail Yakshin <greycat@altlinux.org>
2013-07-22 20:12:58 +0400

    Added Quantcast grabber

---
+++ grabber/quantcast.rb
@@ -0,0 +1,44 @@
+# -*- coding: UTF-8 -*-
+
+require 'cgi'
+require 'uri'
+require 'json'
+require 'grabberutils'
+
+class Quantcast
+	include GrabberUtils
+
+	def run(url)
+		uri = URI.parse(url)
+		host = uri.host
+
+		r = {}
+
+		# Get auth cookies
+		doc = download("https://www.quantcast.com/#{host}")
+		if doc =~ /<td class="reach" id="reach-(.*?)">/
+			r[:id] = id = $1
+		else
+			return nil
+		end
+
+		# Use auth cookies with API call
+		id_encoded = CGI::escape(id)
+		doc = download(
+			"https://www.quantcast.com/api/profile/traffic/?&wUnit=#{id_encoded}&country=US&period=DAY30&countType=",
+			'UTF-8',
+			'Referer' => 'https://www.quantcast.com/#{host}',
+			'X-Requested-With' => 'XMLHttpRequest',
+		)
+
+		d = JSON.load(doc).first
+#		points = d['reach']['US']['PEOPLE']['WEB']
+#		points.each { |pt|
+#			puts Time.at(pt['timestamp']).to_s + "\t" + pt['reach'].to_s
+#		}
+
+		r[:visitors_mon] = d['summaries']['US']['PEOPLE']['WEB']['reach'].to_i
+
+		return r
+	end
+end
