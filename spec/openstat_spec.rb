1b198d3fbfd86d686826e0482e39e94f90cd1ac1
Mikhail Yakshin <greycat@altlinux.org>
2013-07-23 23:36:14 +0400

    Added Openstat tests

---
+++ spec/openstat_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+def check_sanity(res)
+  res[:visitors_day].should_not be_nil
+  res[:visits_day].should_not be_nil
+  res[:pv_day].should_not be_nil
+
+  res[:visitors_mon].should_not be_nil
+  res[:visits_mon].should_not be_nil
+  res[:pv_mon].should_not be_nil
+end
+
+describe Openstat do
+  it 'should parse utro.ru' do
+    res = Openstat.new.run('http://utro.ru/')
+    res[:id].should == 13838
+    check_sanity(res)
+  end
+end
