26e9e162ac33f5812f83dd95a4ccf5c11797e310
Mikhail Yakshin <greycat@altlinux.org>
2014-10-09 04:42:46 +0400

    Removed Alexa non-direct parsing code - regular open edition of Alexa no longer provides any percentages (such as daily % reach) or other estimation hints on all sites. Probably it's still available for paid users, but it kind of kills the purpose. Directly measured ("certified") values still work, but the number of sites that directly employ Alexa's counters *and* publish the results is quite low :(

--- lib/web_analytics_discovery/grabber/alexa.rb
+++ lib/web_analytics_discovery/grabber/alexa.rb
@@ -2,41 +2,23 @@
 
 require 'uri'
 require 'web_analytics_discovery/grabberutils'
-require 'web_analytics_discovery/grabber/openstat'
 
 module WebAnalyticsDiscovery
 class Alexa
 	include GrabberUtils
 
-	BIG_SITE_HOST = 'rutracker.org'
-
-	def initialize
-		@big_perc = get_global_percents(BIG_SITE_HOST)
-		@big_val = Openstat.new.run_id(Openstat::BIG_SITE_ID)
-	end
-
 	def run(url)
 		uri = URI.parse(url)
 		host = uri.host
-		return calc_values(get_global_percents(host))
-	end
-
-	def get_global_percents(host)
 		r = {}
 		doc = download("http://www.alexa.com/siteinfo/#{host}#trafficstats")
 
-		# Try to extract certified metrics first
+		# Try to extract certified metrics
 		r[:visitors_day], r[:pv_day], r[:visitors_mon], r[:pv_mon] = grab_certified_metrics(doc)
 
-		# Try percentages if not certified - it's better than nothing
+		# Grab ID for clarity's sake
 		if doc =~ /<img src="http:\/\/traffic\.alexa\.com\/graph\?.*&u=([^"]+)">/
 			r[:id] = $1
-		end
-		if doc =~ /<table class="visitors_percent">(.*?)<\/table>/m
-			r[:visitors_week_percent], r[:visitors_mon_percent], r[:visitors_mon3_percent] = grab_percentages($1)
-		end
-		if doc =~ /<table class="pageviews_percent">(.*?)<\/table>/m
-			r[:pv_week_percent], r[:pv_mon_percent], r[:pv_mon3_percent] = grab_percentages($1)
 		end
 		return r
 	end
@@ -45,28 +27,6 @@ class Alexa
 		r = []
 		doc.gsub(/<strong class="metrics-data">([0-9,]+)<\/strong>/) { r << $1 }
 		r.map! { |x| x.gsub(/,/, '').to_i }
-		return r
-	end
-
-	def grab_percentages(doc)
-		r = []
-		doc.gsub(/<td class="avg ">([0-9.]+)%<\/td>/) { |x| r << $1.to_f }
-		return r
-	end
-
-	def calc_values(r)
-		# Don't calculate anything if we have no info from Alexa
-		return r unless r[:visitors_mon_percent]
-
-		# Don't calculate anything if we have true certified data
-		return r if r[:visitors_day]
-
-		r[:visitors_mon] = (@big_val[:visitors_mon] / @big_perc[:visitors_mon_percent] * r[:visitors_mon_percent]).to_i
-		r[:pv_mon] = (@big_val[:pv_mon] / @big_perc[:pv_mon_percent] * r[:pv_mon_percent]).to_i
-
-		# Approximate average daily PV from monthly PV
-		r[:pv_day] = (r[:pv_mon] / AVG_DAYS_IN_MONTH).to_i
-
 		return r
 	end
 end
--- spec/alexa_spec.rb
+++ spec/alexa_spec.rb
@@ -1,15 +1,6 @@
 require 'spec_helper'
 
 describe Alexa do
-  it 'should parse utro.ru' do
-    res = Alexa.new.run('http://utro.ru/')
-    res[:id].should == 'utro.ru'
-
-    res[:pv_day].should_not be_nil
-    res[:visitors_mon].should_not be_nil
-    res[:pv_mon].should_not be_nil
-  end
-
   it 'should parse alexa.com with certified metrics' do
     res = Alexa.new.run('http://alexa.com/')
     res[:id].should == 'alexa.com'
